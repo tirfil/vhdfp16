5f677b1929de27c368a7b3af2810fffdae94d790
Philippe THIRION <philippe.thirion@laposte.net>
2020-05-03 22:03:19 +0200

    upgrade mult - discrease # cycle

--- VHDL/fp16mult_v0.vhd
+++ VHDL/fp16mult_v0.vhd
@@ -31,34 +31,22 @@ signal M1,M2,M3 : unsigned(10 downto 0);
 signal X1,X2,X3 : unsigned(4 downto 0);
 signal S1,S2,S3	: std_logic;
 signal MR : unsigned(21 downto 0);
---signal i : integer range 0 to 15;
-
-function to_left(vec : unsigned) return integer is
-variable I : integer range 10 downto 0;
-begin
-	I := 0;
-	while vec(vec'left - I)='0' and I/=vec'left-vec'right loop
-		I:= I+1;
-	end loop;
-	return I;
-end function;
-
+signal i : integer range 0 to 15;
 begin
 
 	P_OTO: process(MCLK, nRST)
-	variable value : integer range 0 to 10;
 	begin
 		if (nRST = '0') then
 			DONE <= '0';
 			OUT0 <= (others=>'0');
-			--M3 <= (others=>'0');
+			M3 <= (others=>'0');
 			X3 <= (others=>'0');
 			S3 <= '0';
 		elsif (MCLK'event and MCLK = '1') then
 			if (state = S_IDLE) then
 				DONE <= '0';
 				OUT0 <= (others=>'0');
-				--M3 <= (others=>'0');
+				M3 <= (others=>'0');
 				X3 <= (others=>'0');
 				S3 <= '0';
 				if (START='1') then
@@ -82,32 +70,22 @@ begin
 					--X3 <= X1 + X2 - 14;
 				--end if;
 				state <= S_1;
-				--i <= 0;
+				i <= 0;
 			elsif ( state = S_1 ) then
 				if (MR(21) = '1') then
-					--M3 <= MR(21 downto 11);
+					M3 <= MR(21 downto 11);
 					state <= S_2;
-				elsif MR(21 downto 11) = "00000000000" then
-					--M3 <= (others=>'0');
-					S3 <= '0';
+				elsif (i = mantissa) then
+					M3 <= (others=>'0');
 					state <= S_2;
 				else
-					value := to_left(MR(21 downto 11));
-					MR <= shift_left(MR,value);
-					X3 <= X3 - value;
-					state <= S_2;
-				--elsif (i = mantissa) then
-					--M3 <= (others=>'0');
-					--state <= S_2;
-				--else
-					----report "SHIFT";
-					--MR <= shift_left(MR,1);
-					--i <= i + 1;
-					--X3 <= X3 - 1;
+					--report "SHIFT";
+					MR <= shift_left(MR,1);
+					i <= i + 1;
+					X3 <= X3 - 1;
 				end if;
 			elsif ( state = S_2 ) then
-				--OUT0 <= S3 & std_logic_vector(X3) & std_logic_vector(M3(9 downto 0));
-				OUT0 <= S3 & std_logic_vector(X3) & std_logic_vector(MR(20 downto 11));
+				OUT0 <= S3 & std_logic_vector(X3) & std_logic_vector(M3(9 downto 0));
 				DONE <= '1';
 				state <= S_IDLE;
 			end if;
